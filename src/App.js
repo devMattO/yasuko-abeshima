b2c0ccb043a0117eaf8902d128a8410720303502
Matt Olsen <olsen_matthew@yahoo.com>
2017-01-18 12:38:59 -0800

    styling landing page and adding content

--- src/App.js
+++ src/App.js
@@ -12,9 +12,14 @@ class App extends Component {
             <h2>Yasuko Abeshima</h2>
           </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="intro-container">
+          <p className="App-intro">
+            Born in Japan, Yasuko Abeshima started her art training in drawing and oil painting in middle school, continued it in high school, and at Gakushuin University in Tokyo and at the University of Wyoming.
+          </p>
+          <p className="App-intro">
+            After moving to Hawaiʻi, she explored media other than watercolor in classes at the University of Hawaiʻi, at the Honolulu Academy of Arts, and in various workshops; in the end, however, she found watercolor to be the most intersting and challenging medium. She became a member of the Hawaiʻi Watercolor Society in 1986, acheiving the signiture status in 1991.
+          </p>
+        </div>
       </div>
     );
   }
